10dfc568cdfd4717464ba997d3b7c459a7387292
Ali El-Cheikh <50736129+Blipblopblopblop@users.noreply.github.com>
2024-09-25 13:06:56 +1000

    Create Api_basic.py

---
+++ greenbone/Example_Scripts/Api_basic.py
@@ -0,0 +1,29 @@
+import sys
+from gvm.connections import TLSConnection
+from gvm.protocols.latest import Gmp
+from gvm.xml import pretty_print
+from gvm.transforms import EtreeCheckCommandTransform
+from gvm.errors import GvmError
+
+connection = TLSConnection(hostname='100.74.219.96', port=9390)
+transform = EtreeCheckCommandTransform()
+
+username = 'ReplaceThis'
+password= 'Replacethis'
+
+try:
+    tasks = []
+
+    with Gmp(connection=connection, transform=transform) as gmp:
+        gmp.authenticate(username, password)
+
+        tasks = gmp.get_tasks(filter_string='name~weekly')
+
+        for task in tasks.xpath('task'):
+            print(task.find('name').text)
+
+except GvmError as e:
+    print('An error occurred', e, file=sys.stderr)
+
+
+
